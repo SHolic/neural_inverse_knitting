200c0e29a299fcf517ffc6c6f1a7197a02f0a3bb
Haoliang Sheng <53325240+SHolic@users.noreply.github.com>
2026-02-16 18:23:39 -0500

    Update dataset download URL to DOI link

--- dataset.sh
+++ dataset.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download dataset
-wget https://figshare.com/ndownloader/files/52098467 -O dataset.zip
+wget https://doi.org/10.6084/m9.figshare.28333379.v2 -O dataset.zip
 
 # Extract to current directory (has dataset/ inside)
 unzip dataset.zip
